f6b5938d8f25a43c69b1de42843063f5528a81e9
sangsoo <tkdtn2013@gmail.com>
2024-11-28 18:32:02 +0900

    초기 파일 업로드

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppBoxSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AppBoxSDK",
+            targets: ["AppBoxSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppBoxSDK",
+            path: "./Sources/AppBoxSDK/AppBoxSDK.xcframework"
+        )
+    ]
+)
