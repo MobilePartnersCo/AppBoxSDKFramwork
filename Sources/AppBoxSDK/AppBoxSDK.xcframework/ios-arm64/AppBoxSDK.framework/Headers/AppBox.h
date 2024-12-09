d4b5fd47cd77baf4a6c728c9480eed67c5f8e723
sangsoo <tkdtn2013@gmail.com>
2024-12-09 17:52:55 +0900

    spm test

--- Sources/AppBoxSDK/AppBoxSDK.xcframework/ios-arm64/AppBoxSDK.framework/Headers/AppBox.h
+++ Sources/AppBoxSDK/AppBoxSDK.xcframework/ios-arm64/AppBoxSDK.framework/Headers/AppBox.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char AppBoxVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AppBox/PublicHeader.h>
 
+#import <AppBoxSDK/FSCalendar.h>
