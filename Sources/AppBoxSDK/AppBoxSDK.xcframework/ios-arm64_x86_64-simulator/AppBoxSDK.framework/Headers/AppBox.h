894d29856e85a5867ff7546777a85d0e8c7b61ee
sangsoo <tkdtn2013@gmail.com>
2024-12-09 14:47:13 +0900

    spm test

---
+++ Sources/AppBoxSDK/AppBoxSDK.xcframework/ios-arm64_x86_64-simulator/AppBoxSDK.framework/Headers/AppBox.h
@@ -0,0 +1,17 @@
+//
+//  AppBox.h
+//  AppBox
+//
+//  Created by mobilePartners on 7/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for AppBox.
+FOUNDATION_EXPORT double AppBoxVersionNumber;
+
+//! Project version string for AppBox.
+FOUNDATION_EXPORT const unsigned char AppBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppBox/PublicHeader.h>
