93e1b058ad84651fea2de0540f43af5a3606c573
sangsoo <tkdtn2013@gmail.com>
2025-02-04 14:29:34 +0900

    구조 변경

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "AppBoxSDK",
-            targets: ["AppBoxSDK"]
+            targets: ["AppBoxPushWrapper"]
         ),
         
         .library(
