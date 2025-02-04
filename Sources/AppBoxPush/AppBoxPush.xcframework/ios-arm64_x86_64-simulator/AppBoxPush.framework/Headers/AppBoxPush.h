5ac9abccdcda375f147fca70a604077b34df9ff6
sangsoo <tkdtn2013@gmail.com>
2025-02-04 18:02:24 +0900

    module test

--- Sources/AppBoxPush/AppBoxPush.xcframework/ios-arm64_x86_64-simulator/AppBoxPush.framework/Headers/AppBoxPush.h
+++ Sources/AppBoxPush/AppBoxPush.xcframework/ios-arm64_x86_64-simulator/AppBoxPush.framework/Headers/AppBoxPush.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char AppBoxPushVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <appboxpush/PublicHeader.h>
 
-
+#import <AppBoxPush/Firebase.h>
