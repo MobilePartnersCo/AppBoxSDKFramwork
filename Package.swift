337f746a1340cb493d0ef1f60b2666c7f7b0aba4
mobpa-jw <jw.jeong@mobpa.co.kr>
2025-11-28 16:17:32 +0900

    1.0.44-beta5

--- Package.swift
+++ Package.swift
@@ -11,42 +11,53 @@ let package = Package(
     products: [
         .library(
             name: "AppBoxSDK",
-            targets: ["AppBoxSDK"]
+            targets: ["AppBoxSDKWrapper"]
         ),
         .library(
             name: "AppBoxHealthSDK",
             targets: ["AppBoxHealthSDK"]
-        ),
-        .library(
-            name: "AppBoxPushSDK",
-            targets: ["AppBoxPushSDK"]
         )
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            from: "11.12.0"
-        )
+        // Firebase
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.12.0"),
+        // Google Sign-In
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "9.0.0"),
+        // Kakao SDK
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", from: "2.26.0"),
+        // Naver SDK
+        .package(url: "https://github.com/naver/naveridlogin-sdk-ios-swift", from: "5.1.0")
     ],
     targets: [
+        // 바이너리
         .binaryTarget(
             name: "AppBoxSDK",
-            path: "./Sources/AppBoxSDK/AppBoxSDK.xcframework"
+            path: "Sources/AppBoxSDK/AppBoxSDK.xcframework"
+        ),
+        // Wrapper
+        .target(
+            name: "AppBoxSDKWrapper",
+            dependencies: [
+                "AppBoxSDK",
+                // Firebase
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                // Google
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                // Kakao
+                .product(name: "KakaoSDKCommon", package: "kakao-ios-sdk"),
+                .product(name: "KakaoSDKAuth", package: "kakao-ios-sdk"),
+                .product(name: "KakaoSDKUser", package: "kakao-ios-sdk"),
+                // Naver
+                .product(name: "NidThirdPartyLogin", package: "naveridlogin-sdk-ios-swift")
+            ],
+            path: "Sources/AppBoxSDK/Sources/AppBoxSDKWrapper"
         ),
         .target(
             name: "AppBoxHealthSDK",
             path: "Sources/AppBoxHealthSDK",
             resources: [.process("Resources/PrivacyInfo.xcprivacy")]
-        ),
-        .target(
-            name: "AppBoxPushSDK",
-            dependencies: [
-                "AppBoxSDK",
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
-            ],
-            path: "Sources/AppBoxPushSDK",
-            resources: [.process("Resources/PrivacyInfo.xcprivacy")]
         )
     ]
 )
-
