61e62afc326b7b7faf2a2eb6417627c96e147635
mobpa-jw <jw.jeong@mobpa.co.kr>
2025-12-11 17:07:21 +0900

    feat: AppBoxSnsLoginSDK 포함 배포

--- Package.swift
+++ Package.swift
@@ -20,12 +20,28 @@ let package = Package(
         .library(
             name: "AppBoxPushSDK",
             targets: ["AppBoxPushSDK"]
+        ),
+        .library(
+            name: "AppBoxSnsLoginSDK",
+            targets: ["AppBoxSnsLoginSDK"]
         )
     ],
     dependencies: [
         .package(
             url: "https://github.com/firebase/firebase-ios-sdk.git",
             from: "11.12.0"
+        ),
+        .package(
+            url: "https://github.com/google/GoogleSignIn-iOS",
+            from: "9.0.0"
+        ),
+        .package(
+            url: "https://github.com/kakao/kakao-ios-sdk",
+            from: "2.26.0"
+        ),
+        .package(
+            url: "https://github.com/naver/naveridlogin-sdk-ios-swift",
+            from: "5.1.0"
         )
     ],
     targets: [
@@ -46,6 +62,19 @@ let package = Package(
             ],
             path: "Sources/AppBoxPushSDK",
             resources: [.process("Resources/PrivacyInfo.xcprivacy")]
+        ),
+        .target(
+            name: "AppBoxSnsLoginSDK",
+            dependencies: [
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "KakaoSDKUser", package: "kakao-ios-sdk"),
+                .product(name: "KakaoSDKAuth", package: "kakao-ios-sdk"),
+                .product(name: "KakaoSDKCommon", package: "kakao-ios-sdk"),
+                .product(name: "NidThirdPartyLogin", package: "naveridlogin-sdk-ios-swift")
+            ],
+            path: "Sources/AppBoxSnsLoginSDK"
         )
     ]
 )
