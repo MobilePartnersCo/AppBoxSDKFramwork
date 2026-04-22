46914203c6e331783c20f0bb42cbd6e18ddb32a7
mobpa-jw <jw.jeong@mobpa.co.kr>
2026-04-22 11:48:49 +0900

    test

---
+++ Sources/AppBoxCoreSDK/AppBoxCoreSDK.xcframework/ios-arm64_x86_64-simulator/AppBoxCoreSDK.framework/Headers/AppBoxCoreSDK.h
@@ -0,0 +1,11 @@
+//
+//  AppBoxCoreSDK.h
+//  AppBoxCoreSDK
+//
+//  Created by mobilePartners on 4/20/26.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double AppBoxCoreSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char AppBoxCoreSDKVersionString[];
